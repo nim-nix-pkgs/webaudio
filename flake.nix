0a50291283c4824d5baa6f3237272a004150c943
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:47 -0300

    chore: re index webaudio

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."webaudio-master".dir   = "master";
+  inputs."webaudio-master".owner = "nim-nix-pkgs";
+  inputs."webaudio-master".ref   = "master";
+  inputs."webaudio-master".repo  = "webaudio";
+  inputs."webaudio-master".type  = "github";
+  inputs."webaudio-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."webaudio-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
